51c70b5e5d47dd0b96dfa8a327782a6900cebb3e
Jeiner Hurtado <127213963+JeinerHI@users.noreply.github.com>
2025-10-29 11:27:03 +0000

    Fragmentación header en php

---
+++ html/header.php
@@ -0,0 +1,37 @@
+<header>
+        <div class="navBar">
+            <a href="Home.html" class="logo">
+                <img src="../img/logo-header.png" alt="Logo">
+            </a>
+            <nav>
+                <ul>
+                    <li class="enlace">
+                        <a href="Contact.html">Contacto</a>
+                    </li>
+                    <li class="enlace">
+                        <a href="Services.html">Servicios</a>
+                        <ul class="desplegable">
+                            <li>
+                                <a href="">Personal Training</a>
+                            </li>
+                            <li>
+                                <a href="">Personal Training + Dieta</a>
+                            </li>
+                            <li>
+                                <a href="">Dieta</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="enlace">
+                        <a href="Testimonials.html">Testimonios</a>
+                    </li>
+                    <li class="enlace">
+                        <a href="Sign.html">Mi Area</a>
+                    </li>
+                </ul> 
+            </nav>
+            <a href="SignUp.html" class="cta1">
+                    <h2>¡Unirme!</h2>
+            </a>
+        </div>
+    </header>
