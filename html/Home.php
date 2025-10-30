bd3c0aed6241c19ddbf43fa6d68f9d70731adb55
JeinerHI <jeinerhur@gmail.com>
2025-10-30 10:06:53 +0100

    Footer a php

--- html/Home.php
+++ html/Home.php
@@ -167,60 +167,7 @@
         </div>
     </section>
 
-    <footer>
-        <div class="whatsapp-button">
-            <div id="chatbox" class="form">
-                <div class="head">
-                    <h3>Chateanos...</h3>
-                    <div id="close" class="close">
-                        <p>x</p>
-                    </div>
-                </div>
-                <textarea name="Escribe un whastapp" id="" placeholder="Hello world"></textarea>
-            </div>
-            <div id="chat-toggle" class="w-button">
-                <img src="../img/whatsapp-logo.webp" alt="">
-            </div>
-        </div>
-        <div class="content-footer">
-            <div class="first-column-footer logo">
-                <img src="../img/logo-header.png" alt="">
-            </div>
-            <div class="second-column-footer">
-                <h3>Personal Trainer</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur. Ut a mattis eget augue vivamus parturient eu aliquet. In
-                        laoreet risus ut nulla interdum urna sollicitudin. Nisl viverra in id ultricies.</p>
-            </div>
-            <div class="third-column-footer">
-                <h3>Menu</h3>
-                <ul>
-                    <li>
-                        <a href="#">
-                            <p>Home</p>
-                        </a>
-                        <a href="../html/Services.html">
-                            <p>Services</p>
-                        </a>
-                        <a href="../html/Contact.html">
-                            <p>Contacto</p>
-                        </a>
-                        <a href="../html/Testimonials.html">
-                            <p>Testimonios</p>
-                        </a>
-                        <a href="Sign.html">
-                            <p>Mi Area</p>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-        <div class="copyright">
-            <p>&copy; 2025. All rights reserved</p>
-        </div>
-
-        <script src="../js/chatbox.js"></script>
-    </footer>
-    
+    <?php require_once("footer.php"); ?>
 
 </body>
 
